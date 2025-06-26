678828c9ec6cda48fe83761c23907a6b6092ece2
Fernando Chumba <fernandochumba00gmail.com>
2025-06-26 13:38:08 -0300

    Impl. Dependencias/modelo/productos

---
+++ Dsw2025Tpi.Application/Dtos/ProductModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Application.Dtos;
+public record ProductModel
+{
+    public record Request( //para crear y actualizar productos
+        string Sku,
+        string InternalCode,
+        string Name,
+        string Description,
+        decimal CurrentUnitPrice,
+        int StockQuanty
+        );
+
+    public record Responde(Guid id);//para devolver datos del producto
+}
+
